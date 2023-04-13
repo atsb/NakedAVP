d732838cba60d48bede3501f5df44d61d2bc7f86
atsb <44937323+atsb@users.noreply.github.com>
2023-04-13 14:47:53 +0200

    Bumping amount of saves
    I've always hated this limit, as a kid it was the one thing I hated.  I like to save often so I don't have to replay such huge amounts of the level again if I die.
    
    Easy = 999 (basically unlimited saves)
    Medium = 24 (I think this is a decent amount for a competent player)
    Hard is unchanged, if you're that good, you don't need it :)

--- src/avp/savegame.c
+++ src/avp/savegame.c
@@ -62,8 +62,8 @@ int LoadGameRequest = SAVELOAD_REQUEST_NONE; //slot number of game to be loaded
 int SaveGameRequest = SAVELOAD_REQUEST_NONE; //slot number of game to be saved
 
 
-#define NUM_SAVES_FOR_EASY_MODE 8
-#define NUM_SAVES_FOR_MEDIUM_MODE 4
+#define NUM_SAVES_FOR_EASY_MODE 999
+#define NUM_SAVES_FOR_MEDIUM_MODE 24
 #define NUM_SAVES_FOR_HARD_MODE 2
 
 int NumberOfSavesLeft;
